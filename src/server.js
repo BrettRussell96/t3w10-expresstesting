ccebf3fb7e02da63e9a8672a99cb565fe280a13b
BrettRussell96 <148216281+BrettRussell96@users.noreply.github.com>
2024-06-13 20:11:04 +1000

    feat: error handling middleware in place

--- src/server.js
+++ src/server.js
@@ -6,13 +6,24 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
+app.get("/", (request, response, next) => {
+
+    return next(new Error("Error on purpose from root route"));
+
     response.json({
         message: "Hello world!"
     });
 });
 
+app.use((error, request, response, next) => {
+    response.json({
+        message: "Error occured!",
+        error: error
+    });
+});
+
+
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
